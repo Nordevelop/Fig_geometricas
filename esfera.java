83366466436ca8c21e85d7bedbd4497b01023b63
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2021-12-16 11:08:00 -0300

    Create esfera.java

---
+++ esfera.java
@@ -0,0 +1,13 @@
+package misClases;
+
+import java.utils.*;
+import java.math.*;
+
+   public class Esfera {
+     /*Author Nordevelop
+     *
+     */
+     //Objetos encapsulados con acceso restringido en la clase Main{}
+     private float radio2;
+     //........f.......//
+   }

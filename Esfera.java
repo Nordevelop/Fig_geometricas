0fb1c25515a36d2b2146f92f34bb94f82fdda7e4
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2021-12-23 00:27:09 -0300

    Update and rename esfera.java to Esfera.java

---
+++ Esfera.java
@@ -0,0 +1,24 @@
+package misClases;
+import java.math.*;
+import java.util.*;
+public class Esfera {
+	/*Author Nordevelop
+	 * 
+	 */
+	private float radio2;
+	public Esfera(float v) //clase constructor
+	{
+		this.radio2=v;
+	}
+	public float Dar_Superficie_Esfera() //metodo
+	{
+		return (float) (4*Math.PI*Math.pow(radio2, 2)); //superficie devuelve S= 4πr**2
+	}
+	public float Dar_Volumen_Esfera()
+	{
+		return (float)(4*Math.PI*Math.pow(radio2, 3))/3;  //volumen  devuelve V=(4πr**3) / 3
+	}
+	
+	
+
+}

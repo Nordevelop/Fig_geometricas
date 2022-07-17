102370201d3da219fa3f6e74ba189a6c016c2c9f
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-07-16 23:22:56 -0300

    Update Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -6,9 +6,11 @@ import java.util.*;
 
 public class Circulo {
 	private double radio1;
-	public Circulo (double k)
-	{
+	private Stack<float>Pila_circulo()=null,
+	public Circulo{ // metodo constructor (double k)
+	
 		this.radio1= k;
+		Pila_Circulo = new Stack<float>();
 	}
     public double Dar_Perimetro_Circulo()
     {
